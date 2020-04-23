f2bbda4ce4eaa4fde934c19ea144bfe012d2a9b9
Hamza01541 <Hamza1541@gmail.com>
2020-04-24 03:06:15 +0500

    a

--- src/env.js
+++ src/env.js
@@ -1,8 +1,15 @@
+/**
+ * @see https://www.jvandemo.com/how-to-use-environment-variables-to-configure-your-angular-application-without-a-rebuild/
+ */
 (function (window) {
   window._env = window._env || {};
 
   // API url
   window._env.apiUrl = process.env.testVar;
+  window._env.testENV = ENV;
+  window._env.processEnv = process.env;
+  window._env.directEnv = testVar;
+  
 
   // Whether or not to enable debug mode
   // Setting this to false will disable console output
