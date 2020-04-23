0605ac5eddeb38feea963016efd575f6a5b40a51
Hamza01541 <Hamza1541@gmail.com>
2020-04-24 03:41:34 +0500

    a

--- server.js
+++ server.js
@@ -16,3 +16,23 @@ res.sendFile(path.join(__dirname,'/dist/timevez/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+(function (window) {
+    window._env = window._env || {};
+  
+    // API url
+    window._env.apiUrl = process.env.testVar;
+    window._env.testENV = ENV;
+    window._env.processEnv = process.env;
+    window._env.directEnv = testVar;
+    
+    console.log("***testVar:",testVar)
+    console.log("***process:",process)
+    console.log("***ENV:",ENV)
+    console.log("***window:",window)
+    console.log("***testVar:",testVar)
+  
+    // Whether or not to enable debug mode
+    // Setting this to false will disable console output
+    window._env.enableDebug = true;
+  }(this));
