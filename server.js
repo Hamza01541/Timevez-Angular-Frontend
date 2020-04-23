dd5864b1f068fb8e4f74a6ce20a1485b438585d4
Hamza01541 <Hamza1541@gmail.com>
2020-04-24 01:15:45 +0500

    minor changes

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-/**
- * Followed this link:
- * @see https://codeforgeek.com/deploy-awesome-angular-app-heroku/ 
- * Could also follow:
- * @see https://www.agiratech.com/how-to-deploy-angular-application-to-heroku/
+/** 
+ * Followed this linkto deploy on heroku: @see https://www.agiratech.com/how-to-deploy-angular-application-to-heroku/
+ * To Cancel multiple builds: @see https://stackoverflow.com/questions/47028871/heroku-your-account-has-reached-its-concurrent-build-limit
  */
 
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
-console.log("**app:",app)
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/timevez'));
 
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname,'/dist/timevez/index.html'));
-
-console.log("**App-get:",res)
 });
 
 // Start the app by listening on the default Heroku port
