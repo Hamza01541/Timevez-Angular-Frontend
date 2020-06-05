9f1c81f04625520a5862449278a025cda53c2c4f
Hamza01541 <Hamza1541@gmail.com>
2020-06-05 15:47:30 +0500

    Revert "[testing] added cchart to admin dahboarf"
    This reverts commit dfaffb66481f3506dfcc9a7df21ce09656e53f64.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -16,6 +16,7 @@ export class LoginComponent implements OnInit {
   loginModel: any = { username: "", password: "" }
   logout: boolean = false;
   currentUser: any;
+  employeesRetention: any[] = [];
 
   constructor(
     private alertService: AlertService,
